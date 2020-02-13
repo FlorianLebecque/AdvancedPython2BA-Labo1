15ffb1030c1dc8abe21928dffcb907a108b30159
Florian <Florianlebecque@hotmail.com>
2020-02-13 09:25:13 +0100

    Add function

--- utils.py
+++ utils.py
@@ -3,14 +3,25 @@
 # Author: Sébastien Combéfis
 # Version: February 8, 2018
 
+from scipy import integrate as itg
+
 def fact(n):
 	"""Computes the factorial of a natural number.
-	
+
+
 	Pre: -
 	Post: Returns the factorial of 'n'.
 	Throws: ValueError if n < 0
 	"""
-	pass
+
+	if(n < 0):
+		return ValueError
+
+	v = 0
+	for i in range(0,n):
+		v *= n
+
+	return n
 
 def roots(a, b, c):
 	"""Computes the roots of the ax^2 + bx + x = 0 polynomial.
@@ -19,7 +30,16 @@ def roots(a, b, c):
 	Post: Returns a tuple with zero, one or two elements corresponding
 		to the roots of the ax^2 + bx + c polynomial.
 	"""
-	pass
+	delta = b**2 - (4*a*c)
+	if(delta < 0):
+		return ()
+	elif(delta == 0):
+		return (-b/(2*a))
+	elif(delta > 0):
+		r1 = (-b+(delta)**(1/2))/(2*a)
+		r2 = (-b-(delta)**(1/2))/(2*a)
+
+		return (r1,r2)
 
 def integrate(function, lower, upper):
 	"""Approximates the integral of a fonction between two bounds
@@ -34,7 +54,9 @@ def integrate(function, lower, upper):
 		you'll probably need the 'eval' function to evaluate the function
 		to integrate given as a string.
 	"""
-	pass
+	f = lambda x:eval(function)
+	return itg.quad(f,lower,upper)
+
 
 if __name__ == '__main__':
 	print(fact(5))
