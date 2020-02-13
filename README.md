204b416e62c54cb44c300448d074b7ba347e59c6
Florian <Florianlebecque@hotmail.com>
2020-02-13 09:50:03 +0100

    clear unittest and add readme

---
+++ README.md
@@ -0,0 +1,2 @@
+![Pass test](https://travis-ci.org/FormFlInc/AdvancedPython2BA-Labo1.svg?branch=master)
+
--- test_utils.py
+++ test_utils.py
@@ -9,18 +9,18 @@ class TestUtils(unittest.TestCase):
     def test_fact(self):
         self.assertEqual(fact(5),120)
         # À compléter...
-        pass
+        
     
     def test_roots(self):
         # À compléter...
         self.assertEqual(roots(1,0,1),())
         self.assertEqual(roots(1,1,0),(0,-1))
-        pass
+        
     
     def test_integrate(self):
         # À compléter...
         self.assertAlmostEqual(integrate('x ** 2 - 1', -1, 1),-1.333333,6)
-        pass
+       
 
 if __name__ == '__main__':
     suite = unittest.TestLoader().loadTestsFromTestCase(TestUtils)
